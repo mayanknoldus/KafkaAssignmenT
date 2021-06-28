383f01e82dd30ff9b1e0587507fed85b2e63ef49
mayanknoldus <82813390+mayanknoldus@users.noreply.github.com>
2021-06-28 23:53:09 +0530

    Initial Commit

--- src/main/scala/edu/knoldus/Deserializer.scala
+++ src/main/scala/edu/knoldus/Deserializer.scala
@@ -1,5 +1,32 @@
 package edu.knoldus
 
-class Deserializer {
+import java.util
+
+import org.apache.kafka.common.serialization.Deserializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+
+class Deserializer extends Deserializer[User] {
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+  }
+
+  override def deserialize(topic: String, bytes: Array[Byte]): User = {
+
+    try {
+      val mapper = new ObjectMapper
+      mapper.registerModule(DefaultScalaModule)
+      //      print(bytes,toString)
+      mapper.readValue(bytes, classOf[User])
+    }
+    catch {
+      case ex: Exception => throw new Exception(ex.getMessage)
+    }
+  }
+
+  override def close(): Unit = {
+
+  }
 
 }
