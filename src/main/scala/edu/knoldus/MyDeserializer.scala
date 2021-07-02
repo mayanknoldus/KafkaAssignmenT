c66bfe3562feef0c140a3e0107163e66be8079e3
mayanknoldus <82813390+mayanknoldus@users.noreply.github.com>
2021-07-02 13:28:00 +0530

    Initial commit

--- src/main/scala/edu/knoldus/MyDeserializer.scala
+++ src/main/scala/edu/knoldus/MyDeserializer.scala
@@ -7,18 +7,18 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 
-class Deserializer extends Deserializer[User] {
+class MyDeserializer extends Deserializer[UserMessage] {
 
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
   }
 
-  override def deserialize(topic: String, bytes: Array[Byte]): User = {
+  override def deserialize(topic: String, bytes: Array[Byte]): UserMessage = {
 
     try {
       val mapper = new ObjectMapper
       mapper.registerModule(DefaultScalaModule)
       //      print(bytes,toString)
-      mapper.readValue(bytes, classOf[User])
+      mapper.readValue(bytes, classOf[UserMessage])
     }
     catch {
       case ex: Exception => throw new Exception(ex.getMessage)
