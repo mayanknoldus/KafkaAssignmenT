bb59d89a160bae281ab5ca6dbe2da6cf074279df
mayanknoldus <82813390+mayanknoldus@users.noreply.github.com>
2021-07-02 13:12:01 +0530

    Initial commit

--- src/main/scala/edu/knoldus/UserMessage.scala
+++ src/main/scala/edu/knoldus/UserMessage.scala
@@ -1,19 +1,3 @@
 package edu.knoldus
 
-//{"id":"1","name":"some_name","age":"24","course":"BTech."}
- abstract class UserMessage {
-
-  var id : Int
-  var name : String
-  var age : Int
-  var course : String
-
-  def this(id : Int, name : String, age : Int, course : String){
-    this()
-    this.id = id
-    this.name = name
-    this.age = age
-    this.course = course
-  }
-
-}
+case class UserMessage(id:Int,name:String,age:String,course:String)
