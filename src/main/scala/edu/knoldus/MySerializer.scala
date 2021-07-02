183204a925d3ca3696efce4f55c5305e24649b22
mayanknoldus <82813390+mayanknoldus@users.noreply.github.com>
2021-07-02 13:26:41 +0530

    Initial commit

--- src/main/scala/edu/knoldus/MySerializer.scala
+++ src/main/scala/edu/knoldus/MySerializer.scala
@@ -7,12 +7,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.apache.kafka.common.serialization.Serializer
 
-class Serializer extends Serializer[User] {
+class MySerializer extends Serializer[UserMessage] {
 
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
   }
 
-  override def serialize(topic: String, data: User): Array[Byte] = {
+  override def serialize(topic: String, data: UserMessage): Array[Byte] = {
     try {
       val mapper = new ObjectMapper()
       mapper.registerModule(DefaultScalaModule)
